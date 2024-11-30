031fb98cf8f379d549390b7c6fdc5b3a40b1607a
fitsum belay <fitsum@anyuai.com>
2024-11-30 20:13:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def download():
                         'key': 'FFmpegExtractAudio',
                         'preferredcodec': 'mp3',
                         'preferredquality': '192'
-                        
+                        'ffmpeg_location': os.environ.get('FFMPEG_PATH', '/usr/bin/ffmpeg')
                     }],
                     'outtmpl': os.path.join(temp_dir, '%(id)s.%(ext)s')  # Use unique filename
                 }
